2777263211faa475e0ccbd1733cb8e0ca5e12bea
Yaboku77 <90348922+Yaboku77@users.noreply.github.com>
2023-05-26 20:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import getenv
 API_KEY = "WNWXNV"
 
 if not API_KEY or API_KEY == "WNWXNV" or API_KEY == "WNWXNV":
-    API_KEY = getenv("API_KEY")
+    API_KEY = getenv("WNWXNV")
 
     if not API_KEY:
         raise Exception("Please add your TechZ Api Key in config.py file")
